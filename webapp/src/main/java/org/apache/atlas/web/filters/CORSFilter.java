e99f2ca4fe362e47768a4ab67b22035d5ae9da10
yorkdick <yorkdick@163.com>
2020-07-20 14:45:31 +0800

    修改打包jar

--- webapp/src/main/java/org/apache/atlas/web/filters/CORSFilter.java
+++ webapp/src/main/java/org/apache/atlas/web/filters/CORSFilter.java
@@ -1,30 +1,30 @@
-package org.apache.atlas.web.filters;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class CORSFilter implements Filter {
-
-    private FilterConfig filterConfig;
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        this.filterConfig = filterConfig;
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS");
-        response.addHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header");
-        response.addHeader("Access-Control-Max-Age", "1800");// 30 min
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-}
+//package org.apache.atlas.web.filters;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//public class CORSFilter implements Filter {
+//
+//    private FilterConfig filterConfig;
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        this.filterConfig = filterConfig;
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        response.addHeader("Access-Control-Allow-Origin", "*");
+//        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS");
+//        response.addHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header");
+//        response.addHeader("Access-Control-Max-Age", "1800");// 30 min
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//}
